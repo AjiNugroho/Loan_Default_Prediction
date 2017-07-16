93334086a57ae5ffb1486755e3233d16c3f1adcd
Jihoon Kim <jihoon_kim@outlook.com>
2017-07-16 12:11:47 +0900

    Neural Network Preliminary Modeling

--- main.py
+++ main.py
@@ -6,10 +6,14 @@ Author: Jihoon Kim
 
 # import modules
 import pandas as pd
+import numpy as np
 from feature_eng import trim_features
 from trim_data import drop_null_columns, categorize_target, split_loan_in_progress
 from one_hot_encoding import one_hot_encoder
+from one_hot_encoding import encode_neural_net_y
 from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense
 import feature_index
 
 
@@ -32,3 +36,19 @@ loan_one_hot_encoded = one_hot_encoder(loan)
 y = loan_one_hot_encoded.loan_status_coded
 X = loan_one_hot_encoded.drop("loan_status_coded", axis=1)
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y)
+
+# Neural Network model
+y_train = encode_neural_net_y(y_train)
+y_test = encode_neural_net_y(y_test)
+
+# create model
+model = Sequential()
+model.add(Dense(163, input_dim=325, activation='relu'))
+model.add(Dense(80, activation='softmax'))
+model.add(Dense(3, activation='softmax'))
+# Compile model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(np.array(x_train), np.array(y_train), epochs=10, batch_size=10, verbose=1)
+scores = model.evaluate(np.array(x_test), np.array(y_test), verbose=0)
+print("====================[TEST SCORE]====================")
+print("%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
--- one_hot_encoding.py
+++ one_hot_encoding.py
@@ -5,6 +5,16 @@ Author: Jihoon Kim
 """
 import feature_index
 import pandas as pd
+from keras.utils import np_utils
+from sklearn.preprocessing import LabelEncoder
+
+
+def encode_neural_net_y(y):
+    encoder = LabelEncoder()
+    encoder.fit(y)
+    encoded_y = encoder.transform(y)
+    new_y = np_utils.to_categorical(encoded_y)
+    return new_y
 
 
 def one_hot_encoder(loan):
